c015992e310e6dbb69cb4230323338f72035cf8b
Andrew Meyer <ameyer@modmatic.io>
2020-02-19 06:31:58 -0600

    Add dotmg_cart_samd21e

---
+++ boards/dotmg_cart_samd21e/board_config.h
@@ -0,0 +1,17 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define __SAMD21E18A__ 1
+
+#define VENDOR_NAME "Modmatic, LLC"
+#define PRODUCT_NAME "dotMG Cart (SAMD21E)"
+#define VOLUME_LABEL "DMGBOOT"
+
+#define USB_VID 0x04D8
+#define USB_PID 0xEC62
+
+#define BOARD_ID "dotMG-Cart-SAMD21E-v0"
+
+#define LED_PIN PIN_PA17
+
+#endif
